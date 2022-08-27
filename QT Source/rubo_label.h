1ffc95c9b8074567281980c8c0348d93632c13b5
wu58430 <wu58430@126.com>
2022-08-27 09:21:25 +0800

    first commit

---
+++ QT Source/rubo_label.h
@@ -0,0 +1,37 @@
+#ifndef RUBO_LABEL_H
+#define RUBO_LABEL_H
+#include <QLabel>
+#include "mainwindow.h"
+
+class MainWindow;
+
+class myLabel : public QLabel
+{
+    Q_OBJECT
+public:
+
+     myLabel(QWidget *parent = nullptr);
+
+    //鼠标进入事件
+    void enterEvent(QEvent *event);
+    //鼠标离开事件
+    void leaveEvent(QEvent *event);
+    void mousePressEvent(QMouseEvent *ev);
+
+    void mouseMoveEvent(QMouseEvent *ev);
+
+signals:
+};
+void mouse_init();
+
+
+struct  mouse
+{
+    bool enter;
+    int x,y;
+    int click; //0 1l 2r
+    bool move;
+};
+extern mouse mouse_flag ;
+
+#endif // RUBO_LABEL_H
